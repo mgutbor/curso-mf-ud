af5988a1c114ca4485bfdef5e038e7195b8b1a48
mgutbor <mgutbor@gmail.com>
2024-03-31 20:22:05 +0200

    2.5 - eventos en react

--- 01-react/src/components/Counter.jsx
+++ 01-react/src/components/Counter.jsx
@@ -3,11 +3,19 @@ import { useState } from "react";
 const Counter = () => {
   const [counter, setCounter] = useState(5);
 
+  const handleAddClick = () => {
+    return setCounter(counter + 1);
+  };
+
+  const handleSubtractClick = () => {
+    return setCounter(counter + 1);
+  };
+
   return (
     <>
       <h1>Counter: {counter}</h1>
-      <button>Add</button>
-      <button>Subtract</button>
+      <button onClick={() => handleAddClick()}>Add</button>
+      <button onClick={() => handleSubtractClick()}>Subtract</button>
     </>
   );
 };
