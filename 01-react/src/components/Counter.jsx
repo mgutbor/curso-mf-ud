b80d6708c04dd18878c9e3655ed45c55fa12b782
mgutbor <mgutbor@gmail.com>
2024-03-31 20:18:11 +0200

    2.4 - fragments

--- 01-react/src/components/Counter.jsx
+++ 01-react/src/components/Counter.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 const Counter = () => {
-
   const [counter, setCounter] = useState(5);
 
-  return <h1>Counter: {counter}</h1>
-}
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button>Add</button>
+      <button>Subtract</button>
+    </>
+  );
+};
 
 export default Counter;
