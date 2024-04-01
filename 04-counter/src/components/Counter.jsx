18d3032e2cd88003a80b35835a19c127d688c87b
mgutbor <mgutbor@gmail.com>
2024-04-01 19:25:27 +0200

    3.12 - Dynamic StyledComponents

--- 04-counter/src/components/Counter.jsx
+++ 04-counter/src/components/Counter.jsx
@@ -3,6 +3,15 @@ import { useState } from "react";
 import Button from "./Button";
 
 import "./Counter.css";
+import styled from "@emotion/styled";
+
+const Success = styled.span`
+  color: lime;
+`;
+
+const Error = styled.span`
+  color: red;
+`;
 
 const Counter = () => {
 
@@ -10,7 +19,8 @@ const Counter = () => {
 
   return (
     <div className="app-counter-container">
-      <h2>Counter: {counter}</h2>
+      <h2>
+        Counter: {counter < 0 ? <Error>{counter}</Error> : <Success>{counter}</Success>}</h2>
       <Button text="Subtract" buttonEvent={() => setCounter(counter - 1)} />
       <Button text="Add" buttonEvent={() => setCounter(counter + 1)} />
     </div>
