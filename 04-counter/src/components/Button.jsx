72326fc0f94c60356b3548f640c81378c101effe
mgutbor <mgutbor@gmail.com>
2024-04-01 19:18:24 +0200

    3.12 - StyledComponents - button

--- 04-counter/src/components/Button.jsx
+++ 04-counter/src/components/Button.jsx
@@ -1,9 +1,21 @@
 
+import styled from "@emotion/styled";
 
+const ButtonStyled = styled.button`
+  color: tomato;
+  background-color: yellow;
+  margin-left: 5px;
+  font-size: 1.25rem;
+  &:hover {
+    color: yellow;
+    background-color: tomato;
+    cursor: pointer;
+  }
+`;
 
 const Button = ({text,buttonEvent}) => {
   return (
-    <button onClick={buttonEvent}>{text}</button>
+    <ButtonStyled onClick={buttonEvent}>{text}</ButtonStyled>
   )
 }
 
