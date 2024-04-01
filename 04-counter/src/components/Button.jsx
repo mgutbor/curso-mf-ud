dd04d0be5c354e8149d49490573616d159262445
mgutbor <mgutbor@gmail.com>
2024-04-01 19:09:17 +0200

    3.11 - Subcomponentes en MF

---
+++ 04-counter/src/components/Button.jsx
@@ -0,0 +1,10 @@
+
+
+
+const Button = ({text,buttonEvent}) => {
+  return (
+    <button onClick={buttonEvent}>{text}</button>
+  )
+}
+
+export default Button
--- 04-counter/src/components/Counter.jsx
+++ 04-counter/src/components/Counter.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
+import Button from "./Button";
 
 import "./Counter.css";
 
 const Counter = () => {
+
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="app-counter-container">Counter</div>
+    <div className="app-counter-container">
+      <h2>Counter: {counter}</h2>
+      <Button text="Subtract" buttonEvent={() => setCounter(counter - 1)} />
+      <Button text="Add" buttonEvent={() => setCounter(counter + 1)} />
+    </div>
   )
 }
 
