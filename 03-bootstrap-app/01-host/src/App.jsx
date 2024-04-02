15ffdc5139f2bf97d786be87bdd7bf6d1cdbb581
mgutbor <mgutbor@gmail.com>
2024-04-02 12:31:07 +0200

    5.4 - Estilos del colorPicker

--- 03-bootstrap-app/01-host/src/App.jsx
+++ 03-bootstrap-app/01-host/src/App.jsx
@@ -6,8 +6,14 @@ import ColorPicker from "ColorPicker/ColorPicker";
 import "./index.css";
 
 const App = () => (
-  <div className="container">
-    <ColorPicker />
-  </div>
+  <>
+    <h1 className="text-center bg-dark text-white p-4">Color Picker</h1>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-12 col-md-4">Lista de colores</div>
+        <div className="col-12 col-md-8"><ColorPicker /></div>
+      </div>
+    </div>
+  </>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- 03-bootstrap-app/02-colorpicker/src/components/ColorPicker.jsx
+++ 03-bootstrap-app/02-colorpicker/src/components/ColorPicker.jsx
@@ -1,8 +1,29 @@
-
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
-  )
-}
+    <form>
+      <input
+        type="color"
+        className="form-control"
+        style={{ width: "100%", height: "300px" }}
+        title="Seleccionar un color"
+      />
 
-export default ColorPicker
+      <div className="text-center">
+        <h2 className="mt-3 fw-bolder">
+          <div
+            style={{
+              width: "20px",
+              height: "20px",
+              background: "#312891",
+              display: "inline-block",
+              margin: "0 1rem",
+              borderRadius: "50%",
+            }}
+          ></div>
+        </h2>
+      </div>
+    </form>
+  );
+};
+
+export default ColorPicker;
