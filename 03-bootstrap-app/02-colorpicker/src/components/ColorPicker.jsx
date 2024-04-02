1c5a17653f55c026d9f28370b6078decb803cf3d
mgutbor <mgutbor@gmail.com>
2024-04-02 12:40:38 +0200

    5.5 - Estados reactivos

--- 03-bootstrap-app/02-colorpicker/src/components/ColorPicker.jsx
+++ 03-bootstrap-app/02-colorpicker/src/components/ColorPicker.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 const ColorPicker = () => {
+
+  const [color, setColor] = useState("#000000");
+
+  const handleChangeColor = (e) => {
+    setColor(e.target.value);  
+  }
+
   return (
     <form>
       <input
@@ -6,20 +15,24 @@ const ColorPicker = () => {
         className="form-control"
         style={{ width: "100%", height: "300px" }}
         title="Seleccionar un color"
+        value={color}
+        onChange={handleChangeColor}
       />
 
       <div className="text-center">
         <h2 className="mt-3 fw-bolder">
           <div
             style={{
-              width: "20px",
-              height: "20px",
-              background: "#312891",
+              width: "30px",
+              height: "30px",
+              background: color,
               display: "inline-block",
               margin: "0 1rem",
               borderRadius: "50%",
+              verticalAlign: "middle",
             }}
           ></div>
+          <span className="text-uppercase" style={{ color: color }}>{color}</span>
         </h2>
       </div>
     </form>
