7915e30b496136c2fff9cd8e1484a8b652472cdb
mgutbor <mgutbor@gmail.com>
2024-04-09 20:18:48 +0200

    9.1 - Counter MF

---
+++ 04-dragonball-app/03-Counter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+import Counter from "./components/Counter";
+
+const App = () => (
+  <div className="text-center mt-10">
+    <Counter initialCounter={100} />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
