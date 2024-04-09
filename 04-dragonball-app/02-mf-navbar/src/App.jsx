4e98b96b3ce0de5ef9478e58c0e87ce57c0e0fb2
mgutbor <mgutbor@gmail.com>
2024-04-09 18:33:23 +0200

    8.3 - React router

--- 04-dragonball-app/02-mf-navbar/src/App.jsx
+++ 04-dragonball-app/02-mf-navbar/src/App.jsx
@@ -4,9 +4,10 @@ import ReactDOM from "react-dom";
 import Navbar from "./components/Navbar";
 
 import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Navbar />
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
       <div>Name: 02-mf-navbar</div>
@@ -14,6 +15,6 @@ const App = () => (
       <div>Language: JavaScript</div>
       <div>CSS: Tailwind</div>
     </div>
-  </>
+  </BrowserRouter>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- 04-dragonball-app/02-mf-navbar/src/components/Navbar.jsx
+++ 04-dragonball-app/02-mf-navbar/src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import { NavLink } from "react-router-dom";
+
+const activeClass = ( isActive ) => 
+`px-3 py-2 text-sm font-medium ${
+  isActive
+  ? "bg-gray-900 text-white"
+  : "text-gray-300 hover:bg-gray-700 hover:text-white"
+}`;
+
 const Navbar = () => {
   return (
     <nav className="bg-gray-800">
@@ -7,36 +16,29 @@ const Navbar = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-            <h1 className="tex-lg text-gray-300">Dragonball APP</h1>
+              <NavLink to="/" className="text-lg text-gray-300">
+                Dragonball APP
+              </NavLink>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                <a
-                  href="#"
-                  className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                  aria-current="page"
-                >
-                  Dashboard
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Team
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Calendar
-                </a>
+                <NavLink 
+                  to="/" 
+                  className={({ isActive }) => activeClass(isActive)}>
+                  Home
+                </NavLink>
+
+                <NavLink 
+                  to="/personajes"
+                  className={({ isActive }) => activeClass(isActive)}>
+                  Personajes
+                </NavLink>
+
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) => activeClass(isActive)}>
+                  About
+                </NavLink>
               </div>
             </div>
           </div>
