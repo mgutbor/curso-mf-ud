ce12ac2a09a86f6b96d4d319cbfd43699a1a1c51
mgutbor <mgutbor@gmail.com>
2024-04-10 17:35:07 +0200

    9.7 - Peticiones desde Vue3

--- 04-dragonball-app/04-Cards/src/components/Cards.vue
+++ 04-dragonball-app/04-Cards/src/components/Cards.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1 class="text-center font-bold text-3xl">Código final desde Vue 3</h1>
+  <div>
+    <h1 class="text-center font-bold text-3xl">Dragonball MF Vue3</h1>
+    <div v-for="personaje in personajes.items" :key="personaje.id">
+      {{ personaje.name }} - {{ personaje.race }}
+    </div>
+  </div>
 </template>
+
+<script setup>
+  import { onMounted, ref } from 'vue';
+
+  const personajes = ref([]);
+
+  const getPersonajes = async () => {
+    const resp = await fetch("https://dragonball-api.com/api/characters");
+    const data = await resp.json();
+    personajes.value = data;
+  }  
+
+  onMounted(() => {
+    getPersonajes();
+  });
+</script>
